3bf7e34a1e0cc5aefbb087387618597d41e65c90
tkellehe <mitchell.kelleher@gmail.com>
2015-12-01 22:32:38 -0500

    Developing convert.h
    Used to test design ideas for convert.h.

---
+++ variable_type/dev/convert_type/main.cpp
@@ -0,0 +1,40 @@
+// Test main.cpp for convert.h
+// Example program
+#include <iostream>
+#include <string>
+
+class B {
+public:
+    virtual char hello() {return 'B';}
+};
+
+class C {
+public:
+    virtual int hello() {return 65;}
+};
+
+template< typename TO, typename FROM >
+TO& convert(FROM const& from) {
+    struct parent {};
+
+    struct child : public parent {
+        child(FROM const& t): item(t){}
+        mutable FROM item;
+    };
+
+    struct sibling : public parent {
+        sibling(TO const& t): item(t){}
+        mutable TO item;
+    };
+
+    parent* p = new child(from);
+    return static_cast< sibling const& >(*p).item;
+};
+
+int main()
+{
+    B b;
+    std::cout << convert< C, B >(b).hello() << std::endl;
+    C c;
+    std::cout << convert< B, C >(c).hello() << std::endl;
+}
